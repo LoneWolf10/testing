f0e0514b427c1cb37467417b3ca883abcf51407e
Nicholas Dhanraj <nickjob_dhanraj@hotmail.com>
2019-04-06 01:13:01 -0400

    Create scripts.py

---
+++ scripts.py
@@ -0,0 +1,20 @@
+import pyautogui
+
+print(pyautogui.position())
+
+#pyautogui.easeInBounce)
+pyautogui.moveTo(1114, 469,1, pyautogui.easeOutQuad)
+pyautogui.click(1114,469)
+
+pyautogui.typewrite("nickjob_dhanraj@hotmail.com")
+
+pyautogui.moveTo(1114, 552,0.5, pyautogui.easeOutQuad)
+pyautogui.click(1104,552)
+
+pyautogui.typewrite("Nickisthebest2")
+
+pyautogui.moveTo(1263,681,0.7, pyautogui.easeOutQuad)
+pyautogui.click(1263,681)
+
+pyautogui.moveTo(1263,681,0.7)
+pyautogui.click(1258,792)
